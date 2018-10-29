ee29ad6d0d2d781180d5d026f03de3de9a366c78
carpenlc <carpenlc@ndwdsndwlpgrd01.arn.gov>
2018-10-29 13:28:49 +0000

    Adding script to run RoDProductManager code.

---
+++ parent/bin/ManageRoDProducts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z `which java` ]; then
+  if [ -z ${JAVA_HOME+x} ]; then 
+  	export JAVA_HOME=/usr/java/jdk1.8.0
+  	export PATH=PATH:${JAVA_HOME}/bin
+  fi
+fi
+SCRIPT_DIR=$(dirname `which $0`)
+LIB_DIR="${SCRIPT_DIR}/../CacheManager/target/lib/"
+
+for i in ${LIB_DIR}*.jar; do
+    CLASSPATH=$CLASSPATH:$i
+done
+
+java -cp ${CLASSPATH} mil.nga.rod.util.RoDProductManager
