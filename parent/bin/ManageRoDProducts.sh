7180a1d4c8ec6c27531eade3b8f5c0fa3453debd
Carpenter <carpenlc@ndwdsndwlpgrd01.arn.gov>
2018-11-05 14:13:00 +0000

    Fixing to run in cron.

--- parent/bin/ManageRoDProducts.sh
+++ parent/bin/ManageRoDProducts.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 PATH=${PATH}:/usr/bin
-if [ -z `which java` ]; then
+if [ -z `/usr/bin/which java` ]; then
   if [ -z ${JAVA_HOME+x} ]; then 
   	export JAVA_HOME=/usr/java/jdk1.8.0
   	export PATH=PATH:${JAVA_HOME}/bin
   fi
 fi
-SCRIPT_DIR=$(dirname `which $0`)
+SCRIPT_DIR=$(/usr/bin/dirname `/usr/bin/which $0`)
 LIB_DIR="${SCRIPT_DIR}/../CacheManager/target/lib/"
 
 for i in ${LIB_DIR}*.jar; do
